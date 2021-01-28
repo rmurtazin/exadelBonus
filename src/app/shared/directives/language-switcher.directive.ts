fe82ae11e2b61e7d3b7cd6206017950d41ce328a
margo10011990 <margo10011990@gmail.com>
2021-01-28 12:11:07 +0200

    deleted not used code

--- src/app/shared/directives/language-switcher.directive.ts
+++ src/app/shared/directives/language-switcher.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, ElementRef, Renderer2, OnChanges } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appLanguageSwitcher]',
