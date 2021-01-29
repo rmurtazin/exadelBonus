7232c578798ed88f681e9ac8d611874174ebe14d
Co0oder <braik978@gmail.com>
2021-01-29 11:19:30 +0200

    fix marge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, Provider } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { MapComponent } from '@components/map/map-container/map-container.component';
 import { MapViewComponent } from '@components/map/map-view/map-view.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { ApiService } from '@services/api.service';
 import { MarkersService } from '@services/markers.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { OfficesService } from '@services/offices.service';
 import { BonusesService } from '@services/bonuses.service';
 import { LanguageSwitcherDirective } from './shared/directives/language-switcher.directive';
@@ -17,14 +18,23 @@ import { HeaderComponent } from '@components/header/header.component';
 import { CloseMenuDirective } from './shared/directives/close-menu.directive';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './shared/components/home/home.component';
+import { AuthInterceptor } from '@services/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterceptor
+};
 
 import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { LoginModule } from '@components/login/login.module';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
@@ -45,6 +55,8 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     HeaderComponent,
     CloseMenuDirective,
     HomeComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     HttpClientModule,
@@ -53,6 +65,7 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     BrowserAnimationsModule,
     HttpClientModule,
     LeafletModule,
+    ToastrModule.forRoot(),
     MatButtonModule,
     MatIconModule,
     LoginModule,
@@ -76,6 +89,7 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     LayoutModule,
     FormsModule,
     ReactiveFormsModule,
+    INTERCEPTOR_PROVIDER,
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/services/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { LoginService } from './login.service';
+import { ToasterService } from './toaster.service';
+
+@Injectable()
+export class AuthInterceptor {
+  constructor(
+    private login: LoginService,
+    private router: Router,
+    private toastr: ToasterService
+  ) {
+  }
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+
+    request = request.clone({
+      setParams: {
+        auth: this.login.getToken()
+      }
+    });
+
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log('Error: ', error);
+        if (error.status === 401) {
+          this.toastr.showError('The User Name or Password entered is incorrect.  Please try again.', 'Error');
+        }
+        return throwError(error);
+      })
+    );
+  }
+
+}
