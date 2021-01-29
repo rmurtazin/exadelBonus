988ace5747b985ce5305f18424e7aada284ea927
Tamila Bodnar <tamila.bodnar@gmail.com>
2021-01-29 15:32:10 +0200

    Add header for all pages

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from '@services/login.service';
 import { IUser } from '@interfaces/loginInterface';
 import { Languages } from '../../enums/languages.enum';
+import { Router, RouterEvent, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +13,17 @@ import { Languages } from '../../enums/languages.enum';
 
 export class HeaderComponent implements OnInit {
   public language = Languages.English;
+  public currentRoute: string;
   isMenuHide = true;
   user: IUser;
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private route: Router) {
+    route.events.pipe(
+      filter((event: Event): event is RouterEvent => event instanceof RouterEvent)
+   ).subscribe((event: RouterEvent) => {
+     console.log(event.url);
+     this.currentRoute = event.url;
+   });
+  }
   ngOnInit(): void {
     this.user = this.loginService.getUser();
   }
@@ -21,4 +31,11 @@ export class HeaderComponent implements OnInit {
   public toggleMenu(): void {
     this.isMenuHide = !this.isMenuHide;
   }
+
+  public onCheckRoute(): boolean{
+    console.log(this.currentRoute);
+    const checkRoutes: String[] = ['/home', '/add-bonus', '/history', '/bonuses', '/users']
+    return checkRoutes.includes(this.currentRoute);
+  } 
+
 }
