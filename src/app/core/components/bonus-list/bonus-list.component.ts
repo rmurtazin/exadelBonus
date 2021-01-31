4ac0a3b126e4e6a8a8c871b365734650d3d769ed
Tamila Bodnar <tamila.bodnar@gmail.com>
2021-01-31 02:39:12 +0200

    Add types

--- src/app/core/components/bonus-list/bonus-list.component.ts
+++ src/app/core/components/bonus-list/bonus-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IBonus } from '../../interfaces/bonus.interface';
 import { BonusesService } from '../../services/bonuses.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bonus-list',
@@ -9,16 +8,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./bonus-list.component.scss'],
 })
 export class BonusListComponent implements OnInit {
-  public bonuses: IBonus[];
+  public bonuses: IBonus[] = [];
   constructor(public bonusesService: BonusesService) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getBonuses();
   }
 
-  public getBonuses(): void{
+  public getBonuses(): void {
     this.bonusesService.getBonuses().subscribe(
-      (data) => {
+      (data: IBonus[]) => {
         if (data) {
           this.bonuses = data;
         }
--- src/app/core/services/bonuses.service.ts
+++ src/app/core/services/bonuses.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { IBonus } from '../interfaces/bonus.interface';
 
 @Injectable()
 export class BonusesService {
   constructor(private http: HttpClient) {}
 
-  public getBonuses(): any {
+  public getBonuses(): Observable<any> {
     return this.http.get('../../../assets/static/bonuses.json');
   }
 }
