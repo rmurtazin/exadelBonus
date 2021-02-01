ecac54755ad5bd862dd0281e07768ee036ac1232
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-02-01 14:53:05 +0200

    feat: add bonus methods

--- src/app/core/services/bonuses.service.ts
+++ src/app/core/services/bonuses.service.ts
@@ -7,7 +7,31 @@ import { ApiService } from '@services/api.service';
 export class BonusesService{
     constructor(private api: ApiService){}
 
+    url = 'assets/static/bonuses.json'; // TODO: change url
+
     public getBonuses(): Observable<IBonus[]> {
-        return this.api.get('assets/static/bonuses.json');
+        return this.api.get(this.url);
+    }
+
+    public addBonus(newBonus: IBonus): Observable<IBonus> {
+      return this.api.post(this.url, newBonus);
+    }
+
+    public removeBonus(id: number): Observable<void> {
+      return this.api.delete(`${this.url}/${id}`);
+    }
+
+    public changeBonus(chBonus: IBonus): Observable<IBonus> {
+      return this.api.put(`${this.url}/${chBonus.id}`, {
+        dateStart: chBonus.dateStart,
+        dateEnd: chBonus.dateEnd,
+        description: chBonus.description,
+        company: chBonus.company,
+        type: chBonus.type,
+        rating: chBonus.rating,
+        discount: chBonus.discount,
+        locations: chBonus.locations,
+        tags: chBonus.tags
+      });
     }
 }
