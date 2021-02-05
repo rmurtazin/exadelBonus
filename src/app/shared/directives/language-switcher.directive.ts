6b4d4ec98d00533eb18251ee3ed9d95a98fc29a4
margo10011990 <margo10011990@gmail.com>
2021-02-05 16:24:16 +0200

    fix toggle bug

--- src/app/shared/directives/language-switcher.directive.ts
+++ src/app/shared/directives/language-switcher.directive.ts
@@ -1,23 +1,34 @@
-import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, OnInit, OnChanges } from '@angular/core';
 import { Languages } from 'src/app/core/enums/languages.enum';
 import { TranslateService } from '@ngx-translate/core';
 
 @Directive({
   selector: '[appLanguageSwitcher]',
 })
-export class LanguageSwitcherDirective {
+export class LanguageSwitcherDirective implements OnInit, OnChanges {
+  public initialLanguage = this.elementRef.nativeElement
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2,
     private translate: TranslateService,
   ) {}
 
+  ngOnChanges (){
+  
+  }
+
+  ngOnInit () {
+  
+  }
+
   @HostListener('click', ['$event'])
   clickEvent(): void {
+    console.log(this.elementRef.nativeElement.textContent)
+   
     const selectedLanguage =
-      this.elementRef.nativeElement.textContent === Languages.English
-        ? Languages.Russian
-        : Languages.English;
+      this.elementRef.nativeElement.textContent === Languages.Russian
+        ? Languages.English
+        : Languages.Russian;
     this.renderer.setProperty(this.elementRef.nativeElement, 'innerHTML', selectedLanguage);
     this.translate.use(selectedLanguage);
   }
