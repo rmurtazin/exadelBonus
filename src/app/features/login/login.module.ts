3a3853e5a098a95aad3689ea13e2aa55892b5279
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-02-09 23:00:00 +0300

    refactor: combine duplicate modules into a shared module

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,28 +1,15 @@
+import { SharedModule } from './../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './login.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    TranslateModule,
     RouterModule.forChild([{ path: '', component: LoginComponent }]),
+    SharedModule,
   ],
   exports: [LoginComponent, RouterModule],
 })
--- src/app/shared/components/datepicker/datepicker.module.ts
+++ src/app/shared/components/datepicker/datepicker.module.ts
@@ -1,19 +1,11 @@
+import { SharedModule } from './../../shared.module';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { DatepickerComponent } from './datepicker.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [DatepickerComponent],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDatepickerModule,
-  ],
+  imports: [MatDatepickerModule, SharedModule],
   exports: [DatepickerComponent],
 })
 export class DatepickerModule {}
