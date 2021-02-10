25d04685884422f84862a42aa5ca75fa613bb3fa
Tamila Bodnar <tamila.bodnar@gmail.com>
2021-02-10 19:19:27 +0200

    run prettier, linter

--- src/app/shared/directives/resize.directive.ts
+++ src/app/shared/directives/resize.directive.ts
@@ -1,28 +1,36 @@
-import { Directive, OnInit, OnDestroy, Input, Renderer2, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  OnDestroy,
+  Input,
+  Renderer2,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Directive({
-  selector: '[onlyForScreen]'
+  selector: '[appResize]',
 })
 export class ResizeDirective implements OnInit, OnDestroy {
-  public listenFunc: Function;
-  @Input('onlyForScreen') configWidth: string;
+  public listenFunc: () => void;
+  @Input('appResize') configWidth: string;
 
   constructor(
     private renderer: Renderer2,
     private templateRef: TemplateRef<any>,
-    private viewContainer: ViewContainerRef
-  ){}
+    private viewContainer: ViewContainerRef,
+  ) {}
 
   public ngOnInit(): void {
     this.displayElement(window.innerWidth);
     this.renderer.listen('window', 'resize', (event) => {
-        this.displayElement(event.target.visualViewport.width);
+      this.displayElement(event.target.visualViewport.width);
     });
   }
 
   public displayElement(visualViewportWidth: number): void {
-    if(this.isValidSize(visualViewportWidth)) {
-      if(!this.viewContainer.get(0)) {
+    if (this.isValidSize(visualViewportWidth)) {
+      if (!this.viewContainer.get(0)) {
         this.viewContainer.createEmbeddedView(this.templateRef);
       }
     } else {
@@ -30,17 +38,16 @@ export class ResizeDirective implements OnInit, OnDestroy {
     }
   }
 
-  isValidSize(viewportWidth: number): boolean {
-    if (this.configWidth === 'mobile'){
+  public isValidSize(viewportWidth: number): boolean {
+    if (this.configWidth === 'mobile') {
       return viewportWidth < 768;
     }
-    if (this.configWidth === 'desktop'){
+    if (this.configWidth === 'desktop') {
       return viewportWidth >= 768;
     }
   }
 
-  ngOnDestroy() {
+  public ngOnDestroy(): void {
     this.listenFunc();
   }
-
 }
