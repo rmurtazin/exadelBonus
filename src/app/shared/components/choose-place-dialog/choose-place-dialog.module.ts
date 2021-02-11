3bac2eee1850b47aebdc65aa5ddd44e15f6799ed
Bohdan <braik978@gmail.com>
2021-02-11 11:51:51 +0200

    prettier fix

--- src/app/shared/components/choose-place-dialog/choose-place-dialog.module.ts
+++ src/app/shared/components/choose-place-dialog/choose-place-dialog.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ChoosePlaceDialogComponent } from './choose-place-dialog.component';
 import { SharedModule } from '../../shared.module';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [ChoosePlaceDialogComponent],
   exports: [ChoosePlaceDialogComponent],
-  imports: [
-    SharedModule,
-    MatRadioModule,
-    MatSelectModule
-  ]
+  imports: [SharedModule, MatRadioModule, MatSelectModule],
 })
-export class ChoosePlaceDialogModule { }
+export class ChoosePlaceDialogModule {}
