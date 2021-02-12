f6c24a7accf8a0deb85707360c62ba5b1f7aca9a
margo10011990 <margo10011990@gmail.com>
2021-02-12 12:09:46 +0200

    corected code

--- src/app/core/services/bonus-address.service.ts
+++ src/app/core/services/bonus-address.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { ApiService } from './api.service';
 
 @Injectable({
