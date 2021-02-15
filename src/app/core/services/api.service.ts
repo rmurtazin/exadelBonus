e31c6544cafc0263f64b5f32a37b74e781b44c4d
margo10011990 <margo10011990@gmail.com>
2021-02-15 11:35:46 +0200

    commit

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -8,16 +8,15 @@ export class ApiService {
   public headers: any = new HttpHeaders().set('Content-Type', 'application/json');
 
   public get(url: string, query?: string): Observable<any> {
-    console.log(`${url}${query || ''}`);
     return this.http.get(`${url}?${query || ''}`);
   }
 
   public post(url: string, body: any): Observable<any> {
-    return this.http.post(url, body, {headers: this.headers});
+    return this.http.post(url, body, { headers: this.headers });
   }
 
   public put(url: string, body: any): Observable<any> {
-    return this.http.put(url, body, {headers: this.headers});
+    return this.http.put(url, body, { headers: this.headers });
   }
 
   public delete(url: string, options?: any): Observable<any> {
