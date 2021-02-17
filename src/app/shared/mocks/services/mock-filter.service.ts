f1bee1838d1fafde5a55e250427b029ced6764f0
Tamila Bodnar <tamila.bodnar@gmail.com>
2021-02-17 14:12:26 +0200

    run linter

--- src/app/shared/mocks/services/mock-filter.service.ts
+++ src/app/shared/mocks/services/mock-filter.service.ts
@@ -1,4 +1,4 @@
-import { FilterService } from "@services/filter.service";
+import { FilterService } from '@services/filter.service';
 
 export const MockFilterService: Partial<FilterService> = {
   addSortToQuery(typeOfSort): string {
