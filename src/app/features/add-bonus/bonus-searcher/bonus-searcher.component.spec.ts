7d8768f385c353733974a4407f0cdd8601056fb8
margo10011990 <margo10011990@gmail.com>
2021-02-20 19:51:24 +0200

    created test files for language-switcher directive and login-component

--- src/app/features/add-bonus/bonus-searcher/bonus-searcher.component.spec.ts
+++ src/app/features/add-bonus/bonus-searcher/bonus-searcher.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { BonusSearcherComponent } from './bonus-searcher.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { EMPTY } from 'rxjs';
 
 describe('BonusSearcherComponent', () => {
   let component: BonusSearcherComponent;
   let fixture: ComponentFixture<BonusSearcherComponent>;
+  const vendor = { id: '17364634765845924816786387641', name: 'MC', email: 'mc@gmail.com' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,5 +24,19 @@ describe('BonusSearcherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onVendorNameChange, should raise the id of vendor name if vendor exist id', () => {
+    component.bonusFormConfig = {
+      closeForm: () => EMPTY,
+      addAddress: () => EMPTY,
+      vendorNameChange: () => EMPTY,
+      createNewVendor: () => EMPTY,
+      createBonus: () => EMPTY,
+      removeVendors: () => EMPTY,
+    };
+    const spy = spyOn(component.bonusFormConfig, 'vendorNameChange').and.callFake(() => vendor);
+    component.getBonusesByVendorId.subscribe((spy) => expect(spy).toEqual(vendor.id));
+    component.onVendorNameChange(vendor);
   });
 });
