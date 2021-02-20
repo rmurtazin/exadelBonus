8825818c849eb2b05f76a9c89d76b18c2ae04301
RasssTor <4etkuu-poz@mail.ru>
2021-02-20 17:21:12 +0300

    remove unused function

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BonusComponent } from '../../shared/components/bonus-list-container/bonus/bonus.component';
 
 @Component({
@@ -6,12 +6,10 @@ import { BonusComponent } from '../../shared/components/bonus-list-container/bon
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   public bonusButtonLabel = 'Apply';
 
   constructor() {}
-
-  ngOnInit(): void {}
 
   public openApplyForm(bonus: BonusComponent): void {}
 }
