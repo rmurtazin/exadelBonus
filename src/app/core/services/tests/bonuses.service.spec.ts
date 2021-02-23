d7bc5232ca26db5a0deaf02ed1ef2a142cab13db
Tamila Bodnar <tamila.bodnar@gmail.com>
2021-02-23 03:47:45 +0200

    rewrite test

--- src/app/core/services/tests/bonuses.service.spec.ts
+++ src/app/core/services/tests/bonuses.service.spec.ts
@@ -1,42 +1,63 @@
-import { TestBed } from '@angular/core/testing';
+import { ApiService } from './../api.service';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpErrorResponse, HttpClientModule } from '@angular/common/http';
 import { asyncData, asyncError } from '../../../shared/mocks/mock-async-observable';
 import { expectedBonuses } from '../../../shared/mocks/constants/bonuses';
 import { IBonus } from '../../interfaces/bonus.interface';
 import { BonusesService } from '../bonuses.service';
 import { ToasterService } from '../toaster.service';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateModule } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
 
 describe('BonusesService', () => {
+  let bonusesService: BonusesService;
+  // const MockApiService: Partial<ApiService> = {
+  //   get(url: string): any {
+  //     return expectedBonuses;
+  //   },
+  // };
+  // const MockToasterService: Partial<ToasterService> = {
+  //   showShow(): void {
+  //     console.log('toastr');
+  //   },
+  // };
+  // let mockApiService: any;
+  // let mockToasterService: any;
+
+
+  let ApiServiceSpy: { get: jasmine.Spy };
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ BonusesService ]
+      providers: [ 
+        BonusesService,
+        { provide: ApiService },
+        { provide: ToasterService }
+      ],
+      imports: [
+        HttpClientTestingModule,
+        HttpClientModule,
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot()
+      ]
     });
+    ApiServiceSpy = jasmine.createSpyObj('ApiService', ['get']);
+    bonusesService =  new BonusesService(<any> ApiService, <any> ToasterService); 
+  });
 
-    let bonusesService: BonusesService = TestBed.inject(BonusesService)
+  it('should be created', () => {
+    let service = TestBed.inject(BonusesService);
+    expect(service).toBeTruthy();
+  });
 
-
-    it('should be OK returning no bonuses',  (done: DoneFn) => {
-      bonusesService.getBonuses().subscribe((data) => {
-        expect(data).toBe(expectedBonuses);
-        done();
-      });
-      bonusesService.getBonuses();
-    
-    
-    
-    // () => {
-    //   spyOn(bonusesService, 'getBonuses').and.callThrough()
-    //   let a = bonusesService.getBonuses();
-
-    //   expect(a).toBe(expectedBonuses, 'should be expectedBonuses')
-
-      // bonusesService.getBonuses().subscribe(
-      //   bonuses => expect(bonuses.length).toEqual(5, 'should have empty bonuses array'),
-      //   fail
-      // );
-
-    });
+  it('should return expected bonuses', () => {
+    ApiServiceSpy.get.and.returnValue(of(expectedBonuses));
+    bonusesService.getBonuses().subscribe(data => 
+      expect(data).toEqual(expectedBonuses, 'expected bonuses'),
+      fail
+    )      
   });
 })
